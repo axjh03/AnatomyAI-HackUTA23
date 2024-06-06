e755fd5ebe7f01d59a4928bfecee33cb07773f5c
Alok Jha <axjh03@gmail.com>
2024-06-06 14:47:28 -0500

    Update downloadLLM.py

--- downloadLLM.py
+++ downloadLLM.py
@@ -4,10 +4,10 @@ import gdown
 # https://drive.google.com/file/d/1UtXGktAaDMCgz4CyjhXVLJy4sz6_XIwx/view?usp=sharing
 
 # File ID
-file_id = "1UtXGktAaDMCgz4CyjhXVLJy4sz6_XIwx"
+file_id = "1c3gkBDnRk0PyonpY_3_6XoIQ90XmEmL0"
 
 # Destination path to save the file
-destination = "llama-2-7b-chat.ggmlv3.q8_0.bin"  # Replace 
+destination = "llama-2-7b-chat.ggmlv3.q4_0.bin"  # Replace 
 
 # Download the file
 gdown.download(f"https://drive.google.com/uc?id={file_id}", destination, quiet=False)
